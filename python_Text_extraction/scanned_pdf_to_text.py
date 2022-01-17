618f111adb9a5cf8a6521e47ad347e92f85c8eee
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2022-01-17 18:27:09 +0100

    Tesseract OCR extractor

--- python_Text_extraction/scanned_pdf_to_text.py
+++ python_Text_extraction/scanned_pdf_to_text.py
@@ -19,10 +19,10 @@ def pdf_to_image(pdf_path: str, dpi: int, lang: str, bad_quali: bool = False) ->
     """
     Function takes pdf scan as input and converts it to single images (one per page).
     Returns amount of pages
-    :param pdf_path:
-    :param dpi:
-    :param lang:
-    :param bad_quali:
+    :param pdf_path: path of the pdf-file
+    :param dpi: Dpi number of the converted pictures from the pdf pages
+    :param lang: Language of Tesseract OCR
+    :param bad_quali: True if the scanned pdfs have a bad quality otherwise False
     :return:
     """
     output_path = pdf_path.strip(".pdf") + "_image_safe"
@@ -44,38 +44,40 @@ def pdf_to_image(pdf_path: str, dpi: int, lang: str, bad_quali: bool = False) ->
         # Save the image of the page in system
         page.save(f"{output_path}/{filename}", 'JPEG')
         if lang == "frk" or bad_quali:
-            preprocess_historical_text(f"{output_path}/{filename}")
+            preprocess_bad_quality_text(f"{output_path}/{filename}")
         # Increment the counter to update filename
         image_counter += 1
     return image_counter, output_path
 
 
-def preprocess_historical_text(output_path):
+def preprocess_bad_quality_text(img_path: str):
+    """
+    :param img_path: path to image for rescale, convert the color from RGB to Gray, erode, dilate and remove/reduce the noises with a filter
+    """
     # https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html
     # https://nanonets.com/blog/ocr-with-tesseract/
-    # base on: https://towardsdatascience.com/getting-started-with-tesseract-part-ii-f7f9a0899b3f
-    # https://towardsdatascience.com/getting-started-with-tesseract-part-i-2a6a6b1cf75e
-    img = cv2.imread(f"{output_path}", cv2.IMREAD_UNCHANGED)
+    # base on: https://towardsdatascience.com/getting-started-with-tesseract-part-ii-f7f9a0899b3f & https://towardsdatascience.com/getting-started-with-tesseract-part-i-2a6a6b1cf75e
+    img = cv2.imread(f"{img_path}", cv2.IMREAD_UNCHANGED)
     img_resize = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
     img_gray = cv2.cvtColor(img_resize, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((1, 1), np.uint8)
     img_dilate = cv2.dilate(img_gray, kernel, iterations=1)
     img_erode = cv2.erode(img_dilate, kernel, iterations=1)
-    img_filter = cv2.threshold(cv2.bilateralFilter(img_erode, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    img_bilateral = cv2.bilateralFilter(img_erode, 5, 75, 75)
+    img_filter = cv2.threshold(img_bilateral, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     img_threshold = cv2.adaptiveThreshold(cv2.bilateralFilter(img_filter, 9, 75, 75), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-    cv2.imwrite(output_path, img_threshold)
+    cv2.imwrite(img_path, img_threshold)
 
 
-def image_to_text(output_path: str, pdf_path: str, file_limit: int, lang: str, out_dir: str) -> None:
+def image_to_text(image_path: str, pdf_path: str, file_limit: int, lang: str, out_dir: str) -> None:
     """
         Function converts images to text using OCR.
-        :param pdf_path:
-        :param file_limit:
-        :param out_dir:
-        :param output_path:
-        :param lang:
-        :return:
-        """
+        :param pdf_path: path to the pdf-file
+        :param file_limit:Number of converted Pages
+        :param out_dir: output directory for the extraction
+        :param image_path: path for the converted pages of the pdf-file
+        :param lang: Language for Tesseract OCR extraction
+    """
     # Variable to get count of total number of pages
 
     # Creating a text file to write the output
@@ -97,7 +99,7 @@ def image_to_text(output_path: str, pdf_path: str, file_limit: int, lang: str, o
             filename = f"page_{i}.jpg"
 
             # Recognize the text as string in image using pytesserct
-            text = str((pytesseract.image_to_string(Image.open(f"{output_path}/{filename}"), lang=lang)))
+            text = str((pytesseract.image_to_string(Image.open(f"{image_path}/{filename}"), lang=lang)))
 
             # The recognized text is stored in variable text
             # Any string processing may be applied on text
@@ -118,10 +120,10 @@ def image_to_text(output_path: str, pdf_path: str, file_limit: int, lang: str, o
     # Delete saved images
     for i in range(1, file_limit):
         filename = f"page_{i}.jpg"
-        os.remove(f"{output_path}/{filename}")
-    if len(os.listdir(output_path)) == 0:
+        os.remove(f"{image_path}/{filename}")
+    if len(os.listdir(image_path)) == 0:
         # removing the file using the os.remove() method
-        os.rmdir(output_path)
+        os.rmdir(image_path)
     else:
         # messaging saying folder not empty
         print("Folder is not empty")
@@ -129,6 +131,10 @@ def image_to_text(output_path: str, pdf_path: str, file_limit: int, lang: str, o
 
 
 def get_all_path_pdf(path_dir: str):
+    """
+    :param path_dir: Directory of the pdf-files
+    :return: set Path of the pdf-Files
+    """
     for file in os.scandir(path_dir):
         if file.is_dir():
             get_all_path_pdf(file)
@@ -140,16 +146,16 @@ def get_all_path_pdf(path_dir: str):
 
 def scanned_pdf_to_text(pdf_path: str, out_name_dir: str, bad_quali: bool, dpi=200, lang="eng"):
     """
-         Quelle:                        https://www.geeksforgeeks.org/python-reading-contents-of-pdf-using-ocr-optical-character-recognition/ +
+         Base-on:                        https://www.geeksforgeeks.org/python-reading-contents-of-pdf-using-ocr-optical-character-recognition/ +
                                         https://gitlab.texttechnologylab.org/Bagci/scannedpdftotext
          Overview possible languages:   https://tesseract-ocr.github.io/tessdoc/Data-Files-in-different-versions.html
-        :param bad_quali:
-        :param out_name_dir:
-        :param pdf_path:
-        :param dpi:
-        :param lang:
-        :return:
-        """
+        :param bad_quali: True if the scanned pdfs have a bad quality otherwise False
+        :param out_name_dir: Directory for the output
+        :param pdf_path: Directory of the pdf
+        :param dpi: Dpi number of the converted pictures from the pdf pages
+        :param lang: Language of Tesseract OCR
+        :return: if the extraction was successfull
+    """
     success = False
     try:
         counter, output_path = pdf_to_image(pdf_path, dpi, lang, bad_quali)
@@ -164,17 +170,18 @@ def scanned_pdf_to_text(pdf_path: str, out_name_dir: str, bad_quali: bool, dpi=2
 def scan_dir_to_text(dir_path: str, out_name_dir: str, bad_quali: bool, dpi: int = 200, lang: str = "deu"):
     """
     Function to convert whole direcotry.
-    :param out_name_dir:
-    :param bad_quali:
-    :param dir_path:
-    :param dpi:
-    :param lang:
-    :return:
+    :param out_name_dir: Directory for the output
+    :param bad_quali: True if the scanned pdfs have a bad quality otherwise False
+    :param dir_path: Directory of all pdf-files
+    :param dpi: Dpi number of the converted pictures from the pdf pages
+    :param lang: Language of Tesseract OCR
     """
     part_func = partial(scanned_pdf_to_text, dpi=dpi, lang=lang, out_name_dir=out_name_dir, bad_quali=bad_quali)
     number_core = int(multiprocessing.cpu_count()/4)
     pool = Pool(number_core)
     # files = [os.path.join(dir_path, file) for file in os.listdir(dir_path)]
+    global set_files
+    set_files = set()
     get_all_path_pdf(dir_path)
     # print(set_files)
     files = list(set_files)
@@ -191,45 +198,12 @@ def scan_dir_to_text(dir_path: str, out_name_dir: str, bad_quali: bool, dpi: int
     print(f"successes: {successes}, fails: {fails}")
 
 
-def get_files_of_txt(input_dir):
-    list_file = []
-    with open(input_dir, "r", encoding="UTF-8") as txt_file:
-        for file_line in txt_file.readlines():
-            name = file_line.replace("\n", "").replace("txt", "pdf")
-            list_file.append(name)
-    return list_file
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path_directory", help="path to the directory with the .pdf files")
     parser.add_argument("-o", "--out", default="pdf_to_txt_out", help="directory path for the outputs of the Tessseract OCR extraction")
-    parser.add_argument("-q", "--quali", default=False, help="Boolean if scanned pdf have a bad quality True otherwise False")
+    parser.add_argument("-q", "--quali", default=False, help="Boolean True if scanned pdf documents have a bad quality otherwise False")
     parser.add_argument("-d", "--dpi", default=200, help="The dpi for converting ever page to a picture")
     parser.add_argument("-l", "--lang", default="deu", help="The language for the Tesseract OCR extraction")
     args = parser.parse_args()
     scan_dir_to_text(dir_path=args.path_directory, out_name_dir=args.out, bad_quali=args.quali, dpi=args.dpi, lang=args.lang)
-    # Comment list are already extracted
-    # BW = ["9", "10", "11"]
-    # NSachs = ["11"]
-    # MV = ["1", "2", "3"]
-    # Nwest = list(range(1, 14))
-    # RF = list(range(7, 14))
-    # Th = ["01"]
-    # Bremen = ["07", "08", "09", "10", "11", "12", "13"]
-    # Sachsen = list(range(1, 4))
-    # Saarland = ["11"]
-    # Hessen = list(range(1, 16))
-    # Bundesrat = ["1949-1950", "1951-1955", "1956-1960", "1961-1965", "1966-1970", "1971-1975", "1976-1980", "1981-1985", "1986-1990", "1991-1995", "1996-2000"]
-    # Bayern
-    # Oesterreich
-    # define Parameter
-    # data_input_dir = "/resources/corpora/parlamentary_germany/Bremen/pdf"
-    # for b in Bremen:
-    #     set_files = set()
-    #     inputs = f"/mnt/hydra/vol/public/biofid"
-    #     print(f"Extracting with OCR from {inputs}")
-    #     frk_lang = "deu"
-    #     dpi_img = 200
-    #     scan_dir_to_text(inputs, dpi_img, frk_lang)
-    #     exit()
