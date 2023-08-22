d14b289e50c27abcad93fc4d93a50425eb5813df
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2023-08-22 22:59:51 +0200

    small updates

--- Java/src/main/java/org/texttechnologylab/parliament/GerParCor.java
+++ Java/src/main/java/org/texttechnologylab/parliament/GerParCor.java
@@ -1,11 +1,36 @@
 package org.texttechnologylab.parliament;
 
+import org.texttechnologylab.parliament.database.MongoDBConfig;
+import org.texttechnologylab.parliament.database.MongoDBConnectionHandler;
+import org.texttechnologylab.parliament.rest.RestHandler;
+import spark.Spark;
 import spark.servlet.SparkApplication;
+
+import java.io.IOException;
 
 public class GerParCor implements SparkApplication {
 
+    public static void main(String[] args) {
+
+    }
+
     @Override
     public void init() {
+        Spark.port(8081);
+        Spark.staticFileLocation("html");
+
+        String sDBConfig = GerParCor.class.getClassLoader().getResource("dbconnection.txt").getPath();
+
+        try {
+            MongoDBConfig dbConfig = new MongoDBConfig(sDBConfig);
+            MongoDBConnectionHandler pHandler = new MongoDBConnectionHandler(dbConfig);
+
+            RestHandler m = new RestHandler(pHandler);
+            m.init();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
--- Java/src/main/java/org/texttechnologylab/parliament/rest/RestHandler.java
+++ Java/src/main/java/org/texttechnologylab/parliament/rest/RestHandler.java
@@ -1,4 +1,19 @@
 package org.texttechnologylab.parliament.rest;
 
+import org.texttechnologylab.parliament.database.MongoDBConnectionHandler;
+
 public class RestHandler {
+
+    private MongoDBConnectionHandler dbConnection = null;
+
+    public RestHandler(MongoDBConnectionHandler dbConnection){
+        this.dbConnection = dbConnection;
+    }
+
+    public static void init(){
+
+    }
+
+
+
 }
