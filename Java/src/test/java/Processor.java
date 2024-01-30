95ed0bdedf375f6efbfaf59c9049968abd764dce
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2024-01-30 10:24:42 +0100

    Analyse Updates

--- Java/src/test/java/Processor.java
+++ Java/src/test/java/Processor.java
@@ -69,6 +69,56 @@ public class Processor {
         composer.shutdown();
 
     }
+    public void example() throws Exception {
+
+        int iScale = 2;
+
+        File pFile = new File(Processor.class.getClassLoader().getResource("rw").getFile());
+
+        MongoDBConfig pConfig = new MongoDBConfig(pFile);
+        String sFilter = "{}";
+
+        DUUIAsynchronousProcessor processor = new DUUIAsynchronousProcessor(new DUUIGerParCorReader(pConfig, sFilter));
+
+        DUUIComposer composer = new DUUIComposer()
+                .withSkipVerification(true)
+                .withWorkers(iScale)
+                .withLuaContext(new DUUILuaContext().withJsonLibrary());
+
+        DUUIDockerDriver dockerDriver = new DUUIDockerDriver();
+        DUUIUIMADriver uimaDriver = new DUUIUIMADriver();
+        DUUIRemoteDriver remoteDriver = new DUUIRemoteDriver();
+        DUUISwarmDriver swarmDriver = new DUUISwarmDriver();
+        composer.addDriver(dockerDriver, remoteDriver, uimaDriver, swarmDriver);
+
+        DUUIPipelineComponent language = new DUUIUIMADriver.Component(createEngineDescription(SetLanguage.class,
+                SetLanguage.PARAM_LANGUAGE, "de")).withScale(iScale).build();
+
+        DUUIPipelineComponent component = new DUUISwarmDriver.Component("docker.texttechnologylab.org/textimager-duui-transformers-sentiment:latest").withScale(iScale)
+                .withParameter("model_name", "oliverguhr/german-sentiment-bert")
+                .withParameter("selection", "text")
+                .build();
+
+        DUUISegmentationStrategy pStrategy = new DUUISegmentationStrategyByDelemiter()
+                .withDelemiter(".")
+                .withLength(300000);
+
+        composer.add(language);
+        composer.add(component);
+//
+//        AnalysisEngineDescription writerEngine = createEngineDescription(GerParCorWriter.class,
+//                GerParCorWriter.PARAM_DBConnection, pFile.getAbsolutePath()
+//        );
+
+        AnalysisEngineDescription countAnnos = createEngineDescription(CountAnnotations.class);
+
+        composer.add(new DUUIUIMADriver.Component(countAnnos).withScale(iScale).build());
+
+        composer.run(processor, "reloaded");
+
+        composer.shutdown();
+
+    }
 
 
 }
